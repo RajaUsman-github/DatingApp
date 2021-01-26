f7126f0f57d38f32c2ad0f5e4f99b42a397031f6
RajaUsman-github <rajau8129@gmail.com>
2021-01-26 22:45:25 +0500

    Section 5 Login(Part1)

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+  loggedIn:boolean;
+
+  constructor(private accountServices: AccountService) { }
+
+  ngOnInit(): void {
+    this.getCurrentUser();
+  }
+login()
+{
+  this.accountServices.login(this.model).subscribe(reponse=>{
+    console.log(reponse);
+    this.loggedIn=true;
+  },error=>{
+    console.log(error);
+  })
+}
+logout(){
+  this.accountServices.logout();
+  this.loggedIn=false;
+}
+getCurrentUser(){
+  this.accountServices.currentUsers$.subscribe(user=>{
+    this.loggedIn=!!user;
+  },error=>{
+    console.log(error);
+  })
+}
+}

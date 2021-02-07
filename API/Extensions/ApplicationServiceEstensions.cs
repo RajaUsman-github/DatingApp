920a38fb81de2b87fc8b6b4eacb4c15f8c1f6178
RajaUsman-github <rajau8129@gmail.com>
2021-02-07 19:40:21 +0500

    End of 8 (extending api)

--- API/Extensions/ApplicationServiceEstensions.cs
+++ API/Extensions/ApplicationServiceEstensions.cs
@@ -1,4 +1,5 @@
 ﻿using API.Data;
+using API.Helper;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
@@ -16,6 +17,8 @@ namespace API.Extensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services,IConfiguration config)
         {
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(typeof(AutoMaperProfiles).Assembly);
             services.AddDbContextPool<DataContext>(options => options.UseSqlServer(config.GetConnectionString("DatingAppConnection")));
             return services;
         }

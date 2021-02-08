7bb8412d95524a9d754f9f9c0f963f02099a79e6
RajaUsman-github <rajau8129@gmail.com>
2021-02-09 01:04:58 +0500

    End of section 9(user interface)

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,13 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-baseUrl='https://localhost:44374/api/';
+// baseUrl='https://localhost:44374/api/';
+baseUrl = environment.apiUrl;
 private currentUserSource=new ReplaySubject<User>(1);
 currentUsers$= this.currentUserSource.asObservable();
 
